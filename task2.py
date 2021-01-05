1f0e9f57407a8b9f656de97096b45c652e67f3b8
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-01-05 17:55:45 +0600

    authorization

--- task2.py
+++ task2.py
@@ -4,6 +4,7 @@ password = '123456'
 def sign_in():
     tries = 0
     while tries < 3:
+
         chek_username = input()
         chek_password = input()
         if chek_username == username and chek_password == password:
