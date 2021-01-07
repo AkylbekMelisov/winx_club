3737ee0dcc701f7f69b6e8b5984972d0a999345b
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-01-07 18:15:24 +0600

    шпаргалка

--- controltask/qwerty.py
+++ controltask/qwerty.py
@@ -1,4 +1,4 @@
-list1 = [2,4,5,7,9,8,6,2,3,4,1,5,1,1,1,1]
+# list1 = [2,4,5,7,9,8,6,2,3,4,1,5,1,1,1,1]
 # list2 = []
 # i = 0
 # while i < len(list1):
@@ -6,4 +6,15 @@ list1 = [2,4,5,7,9,8,6,2,3,4,1,5,1,1,1,1]
 #         list2.append(list1[i])
 #     i += 1
 # print(list2)
-print(set(list1))
+
+# print(set(list1))
+
+# ----------------------
+
+file1 = open('test.txt')
+list1 = file1.readlines()
+# print(list1)
+i = 0
+while i < len(list1):
+    print(list1[i].strip())
+    i += 1
