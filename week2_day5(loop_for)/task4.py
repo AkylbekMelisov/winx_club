3349b14060c851147cea42fe974724f05577faa9
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-01-08 17:29:32 +0600

    dictionary

---
+++ week2_day5(loop_for)/task4.py
@@ -0,0 +1,8 @@
+# dict1 = {"samsung 1":60,"iphone X":4000,"xiomi":8000}
+# print(dict1['iphone X'])
+
+dict1 = {"nurjanat":['pizza','car','house'],
+         "aigerim":['car','penthouse','ticket']
+         }
+dict1['nurjanat'].append(4)
+print(dict1)
