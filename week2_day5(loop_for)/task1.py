98d4de91e463a31ba037345e676eb50d7dc274c0
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-01-08 16:05:35 +0600

    loop for

---
+++ week2_day5(loop_for)/task1.py
@@ -0,0 +1,17 @@
+# list1 = [1,2,3,4]
+# list2 = ['blue','red','white','green']
+
+# for color in list2:
+#     print(color)
+
+# for i in range(100,200):
+#     if i % 2 == 0:
+#         print(i)
+
+# result = 1
+# for i in range(1,10):
+#     result *= i
+# print(result)
+
+# for i in 'blue','red','white','green':
+#     print(i)
---
+++ week2_day5(loop_for)/task2.py
@@ -0,0 +1,16 @@
+# try:
+#     print(1 + '1')
+# except TypeError:
+#     print(1)
+
+# list1 = [1,2,3]
+# try:
+#     print(list1[4])
+# except IndexError:
+#     print(list1[-1])
+
+try:
+    print(1/0)
+except ZeroDivisionError:
+    print(0)
+
