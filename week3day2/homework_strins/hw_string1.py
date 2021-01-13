05f2013acf7e03e4b5843f27ae57ad0f8df06b5b
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-01-13 14:22:59 +0600

    a new version

--- week3day2/homework_strins/hw_string1.py
+++ week3day2/homework_strins/hw_string1.py
@@ -1,5 +1,7 @@
-str1 = "helxlo woxxxrld x"
-if 'x' in str1:
-    print(str1.replace('x',''))
+str1 = "hello hi world"
+x = 'hi'
+
+if x in str1:
+    print(str1.replace(x,''))
 else:
     print('NOT OK')
