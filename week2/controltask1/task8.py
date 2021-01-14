41a5f31c161e91e0bcd0a2af5430a2153467e0c0
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-01-14 15:36:49 +0600

    count str

--- week2/controltask1/task8.py
+++ week2/controltask1/task8.py
@@ -1,4 +1,4 @@
 def open_file(filename,text):
     file1 = open(filename,'w')
     file1.write(text)
-open_file('test.txt','hellow!')
+open_file('test.txt', 'hellow!')
---
+++ week3/day3/hw_strings/hw_string1.py
@@ -0,0 +1,5 @@
+str1 = "Hellsadsadoy world fdssd asdfdsaf ewew dsaf"
+z = str1.count(' ')
+print(z+1)
+
+print(len(str1.split()))
