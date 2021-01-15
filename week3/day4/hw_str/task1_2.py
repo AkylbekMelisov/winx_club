9382f1d25e0a7d0bcff7b77eca4594d5ba7dce0e
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-01-15 11:35:27 +0600

    hw_strings

---
+++ week3/day4/hw_str/task1_2.py
@@ -0,0 +1,11 @@
+str1 = "hello world"
+string1 = str1[0]
+print(string1)
+
+phone_number = int(input("Введите номер телефона ""+"))
+str2 = str(phone_number)
+
+if len(str2) == 10:
+    print('Ok')
+else:
+    print('Введите 10 символов')
---
+++ week3/day4/hw_str/task3_4.py
@@ -0,0 +1,9 @@
+str1 = "Asdad Bsdfsdf Cesfsdf"
+str2 = str1.replace('A','B')
+str3 = str2.replace('C','D')
+# print(str3)
+
+number = 2254425534
+str4 = str(number)
+maximum = max(str4)
+print(maximum)
