a823c3b3d2918465e074dfed7449de3f6490867b
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-01-17 12:21:13 +0600

    like str 'a'(new version)

--- week3/day5/task2.py
+++ week3/day5/task2.py
@@ -1,15 +1,12 @@
-str1 = "xaxxxxa"
+str1 = input()
 if len(str1) // 2 < str1.count('a'):
-    print('любимая строка Алисы')
+    print(len(str1))
 else:
     i = 0
     while len(str1) // 2 >= str1.count('a'):
         if str1[i] != "a":
-            str1 = str1.replace(str1[i],'',1)
+            str1 = str1.replace(str1[i],'',2)
         if i == len(str1)-1:
             break
         i += 1
-    print(str1)
-
-
-
+    print(len(str1))
